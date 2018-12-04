f9d7c45c4ec4c20891ccd6132422291c75096b00
Melissa Real <melissa.real@hotmail.com>
2018-12-04 12:42:30 +0000

    Automatically backed up by Learn

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -6,20 +6,32 @@ class PetsController < ApplicationController
   end
 
   get '/pets/new' do
+    @owners = Owner.all
     erb :'/pets/new'
   end
 
   post '/pets' do
-
+    @pet = Pet.create(params[:pet])
+     if !params["owner"]["name"].empty?
+        @pet.owner_id << Owner.create(name: params["owner"]["name"]).id
+     end
     redirect to "pets/#{@pet.id}"
   end
 
   get '/pets/:id' do
+
     @pet = Pet.find(params[:id])
-    erb :'/pets/show'
+    erb :'pets/show'
   end
 
+
   patch '/pets/:id' do
+    @pet = Pet.find(params[:id])
+    @pet.update(params["pet"])
+
+    if !params["owner"]["name"].empty?
+      @pet.owner = Owner.create(name: params["owner"]["name"])
+    end
 
     redirect to "pets/#{@pet.id}"
   end
